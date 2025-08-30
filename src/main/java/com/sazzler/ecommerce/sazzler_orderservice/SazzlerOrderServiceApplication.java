7c360fadeca7b0de4fa18b131066ddf841e54446
Amodhyh <35001977+amodhyh@users.noreply.github.com>
2025-08-30 23:41:18 +0530

    changed the application.properties and main class

--- src/main/java/com/sazzler/ecommerce/sazzler_orderservice/SazzlerOrderServiceApplication.java
+++ src/main/java/com/sazzler/ecommerce/sazzler_orderservice/SazzlerOrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.sazzler.ecommerce.sazzler_orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SazzlerOrderServiceApplication {
 
     public static void main(String[] args) {
